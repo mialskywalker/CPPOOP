fb6727d8d20c37e391f497b16f39c2e494c7de63
mialskywalker <53373335+mialskywalker@users.noreply.github.com>
2024-08-10 11:38:34 +0200

    total average of students

--- Demo/main.cpp
+++ Demo/main.cpp
@@ -6,65 +6,50 @@
 
 using namespace std;
 
-class Sale {
-    string town;
-    string product;
-    double price;
-    double quantity;
+class Student {
+    string name;
+    string surname;
+    double average;
 
 public:
-    Sale(string town, string product, double price, double quantity):
-        town(town),
-        product(product),
-        price(price),
-        quantity(quantity) {}
+    Student(string name, string surname, double average) :
+        name(name),
+        surname(surname),
+        average(average) {}
 
-    string getTown() { return this->town; }
-    double getProductPrice() { return this->price * this->quantity; }
-};
+    double getAverage() { return this->average; }
 
-class Sales {
-    typedef vector<Sale> SaleData;
-    SaleData data;
-    map<string, double> salesByTown;
-
-public:
-    Sales(istream & istr) {
-        int n;
-        istr >> n;
-        istr.ignore();
-
-        string town, product;
-        double price;
-        double quantity;
-
-        while(n--) {
-            istr >> town >> product >> price >> quantity;
-            data.push_back(Sale(town, product, price, quantity));
-        }
-    }
-
-    void calculateByTown() {
-        salesByTown.clear();
-        for (Sale & cur : data) {
-            const string & town = cur.getTown();
-            salesByTown[town] += cur.getProductPrice();
-        }
-    }
-
-    string printSales(){
+    string printInfo() {
         ostringstream os;
-        os << fixed << setprecision(2);
-        for (pair<string, double> c: salesByTown)
-            os << c.first << " -> " << c.second << endl;
+        os << this->name << ' ' << this->surname << ' ' << this->average << endl;
         return os.str();
     }
-
 };
 
 int main() {
-    Sales sales(cin);
-    sales.calculateByTown();
-    cout << sales.printSales();
+    vector<Student> students;
+
+    int n;
+    cin >> n;
+    if (n < 1) {
+        cout << "Invalid input" << endl;
+        return 0;
+    }
+
+    string name, surname;
+    double avg;
+    while (n--) {
+        cin >> name >> surname >> avg;
+        students.push_back(Student(name, surname, avg));
+    }
+
+    double total = 0;
+    for (Student s : students) {
+        cout << s.printInfo();
+        total += s.getAverage();
+    }
+
+    total /= students.size();
+    cout << total << endl;
 
 }
