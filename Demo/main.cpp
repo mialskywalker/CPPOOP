c01a7955d8607a40eaf4e226ce3af1327cdc86bc
mialskywalker <53373335+mialskywalker@users.noreply.github.com>
2024-08-10 10:05:24 +0200

    sentence shifter

--- Demo/main.cpp
+++ Demo/main.cpp
@@ -1,52 +1,65 @@
 #include <iostream>
+#include <vector>
 #include <string>
 #include <sstream>
-#include <vector>
 
 using namespace std;
 
 class SentenceShifter {
-private:
     typedef vector<string> WordData;
+
     WordData data;
 
 public:
-    void readData(istream & is);
-    void getShiftedSentence(int n);
-    void outData(ostream & os);
+    SentenceShifter(WordData data) : data(data) {}
+
+    SentenceShifter() {
+        data = WordData{};
+    }
+
+    void pushData(string word) {
+        this->data.push_back(word);
+    }
+
+    void shiftData(int times) {
+        while (times--) {
+            string curr = this->data.back();
+            this->data.pop_back();
+            this->data.insert(this->data.begin(), curr);
+        }
+    }
+
+    string printData() {
+        ostringstream os;
+        bool bFirst = true;
+        for (string el : this->data) {
+            if (bFirst) {
+                os << el;
+                bFirst = false;
+            }
+            else {
+                os << endl << el;
+            }
+        }
+        return os.str();
+    }
+
 
 };
 
-void SentenceShifter::readData(istream &is) {
-    string buff;
-    getline(cin, buff);
-    istringstream istr(buff);
-    while(istr >> buff) {
-        data.push_back(buff);
-    }
-}
-
-void SentenceShifter::outData(ostream &os) {
-    for (string el : data)
-        os << el << endl;
-}
-
-void SentenceShifter::getShiftedSentence(int n) {
-    for (int i = 0; i < n; i++) {
-        string word = data.back();
-        data.pop_back();
-        data.insert(data.begin(), word);
-    }
-}
-
-
-
-
 int main() {
+
     SentenceShifter sh;
-    sh.readData(cin);
-    int times;
-    cin >> times;
-    sh.getShiftedSentence(times);
-    sh.outData(cout);
+    string text;
+    getline(cin, text);
+    istringstream istr(text);
+    string word;
+    while (istr >> word)
+        sh.pushData(word);
+
+    int num;
+    cin >> num;
+    sh.shiftData(num);
+    cout << sh.printData();
+
 }
